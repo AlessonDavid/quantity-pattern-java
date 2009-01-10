0687baebbec00bddfee20f610b5cb7c961965046
dexterous <saager.mhatre@gmail.com>
2009-01-10 13:50:20 +0530

    [svn r21] added back job of class; accidentally deleted in refactorings

--- src/com/tw/obc/Length.java
+++ src/com/tw/obc/Length.java
@@ -1,5 +1,6 @@
 package com.tw.obc;
 
+// Understands scale of 1D objects
 public class Length extends ScaledUnit<Length> {
     public static final Length KM = new Length("km", 1000000);
     public static final Length  M = new Length( "m", 1000);
